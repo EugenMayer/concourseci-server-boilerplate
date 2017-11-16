dc65168b7e984f321e60670f788c79940c2d4c73
em <eugen.mayer@kontextwork.de>
2017-11-16 20:40:30 +0100

    use nested vault values

--- run_vault_test.sh
+++ run_vault_test.sh
@@ -1,7 +1,8 @@
 fly -t lite login -c http://127.0.0.1:8080 -u concourse -p changeme || (echo "please login yourself since http://127.0.0.1:8080 is not the right docker-machine ip for you" && exit 1)
 
 echo "inserting value"
-docker-compose exec config bash -l -c 'source /vault/server/init_vars && vault write secret/concourse/main/main/myvalue value=foo'
+docker-compose exec config bash -l -c 'source /vault/server/init_vars && vault write secret/concourse/main/main/firstvalue value=foo'
+docker-compose exec config bash -l -c 'source /vault/server/init_vars && vault write secret/concourse/main/main/nested/secondvalue value=bar'
 
 # deploy the pipline
 fly sp -t lite configure -c examples/vault-based/pipeline.yml  -p main -n
